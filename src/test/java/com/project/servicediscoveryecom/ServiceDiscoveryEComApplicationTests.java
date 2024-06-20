96cf0acc03b118ea4766976844dd22d89595dada
Dhananjay Bhadke <bhadke443@gmail.com>
2024-06-20 19:46:52 +0530

    Service Discovery

---
+++ src/test/java/com/project/servicediscoveryecom/ServiceDiscoveryEComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.servicediscoveryecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryEComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
